1bc877fc8d0a6493ffe0575069d64efdadeb357e
Yuuhi-Hazeki <yuuhi.hazeki.recruit@gmail.com>
2025-02-01 23:45:36 +0900

    在庫追加ページのhtml修正

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -2,11 +2,14 @@ package com.example.demo.model;
 
 
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,11 +23,17 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@Column(nullable = false)
-	private String name;
-	@Column(nullable = false)
-	private int quantity;
-	@Column(nullable = false)
-	private int price;
 	
+	private String name;
+	
+	private int quantity;
+	
+	private int price;
+	 @Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+	 
+	 @PrePersist
+	    protected void onCreate() {
+	        createdAt = LocalDateTime.now();
+	    }
 }
