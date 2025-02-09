8f834165823ba934cbf4217644268c5f24dc35f9
Yuuhi-Hazeki <yuuhi.hazeki.recruit@gmail.com>
2025-02-09 21:02:46 +0900

    細かい検索機能実装途中

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -8,7 +8,16 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-	
+	//商品名の部分一致検索
 	List<Product> findByNameContainingIgnoreCase(String name);
+	
+	//価格範囲で検索
+	List<Product>findByPriceBetween(double minPrice, double maxPrice);
+	
+	//在庫が指定以上の商品のみ取得
+	List<Product>findByQuantityGreaterThanEqual(int minQuantity);
+	
+	//価格順で並べる
+	List<Product> findByNameVontainingOrderByPriceAsc(String name);
 
 }
